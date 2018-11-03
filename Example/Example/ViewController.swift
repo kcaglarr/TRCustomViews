61e7199d44a07d07d3b7b755e54d00edbbeba0d7
kerimcaglar <kerim.caglarr@gmail.com>
2018-11-03 22:09:57 +0300

    created example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by kerimcaglar on 3.11.2018.
+//  Copyright © 2018 kerimcaglar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
